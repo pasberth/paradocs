daf6714d61b3f3acadf92a17720f3bf9fcc73e76
pasberth <pasberth@gmail.com>
2014-04-12 17:36:38 +0900

    paradocs.js ホスティング

---
+++ static/highlightjs/paradocs.js
@@ -0,0 +1,38 @@
+hljs.registerLanguage("paradocs", function(hljs) {
+  return {
+    c: [
+      {
+        cN: 'string',
+        v: [
+          { b: '%\\\\.?' },
+          { b: '%""""', e: '""""' },
+          { b: '%"""', e: '"""' },
+          { b: '%""', e: '""' },
+          { b: '%"', e: '[^\\\\]"' },
+          { b: '\\\\.?' },
+          { b: '""""', e: '""""' },
+          { b: '"""', e: '"""' },
+          { b: '""', e: '""' },
+          { b: '"', e: '[^\\\\]"' },
+        ]
+      },
+      {
+        cN: "keyword",
+        v: [
+          { b: "%rule" },
+          { b: "%extend" },
+          { b: "%def" },
+          { b: "%escape" },
+          { b: "%before" },
+          { b: "%after" },
+          { b: "%include" },
+          { b: "%read" },
+        ]
+      },
+      {
+        cN: 'symbol',
+        b: /%\S+/
+      }
+    ]
+  };
+})
